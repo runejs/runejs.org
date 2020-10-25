069873e60a509e366b11d7a460af4500a3d241dd
TheBlackParade <runejs.dev@gmail.com>
2020-10-25 16:59:00 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rune-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'runejs-web';
+}
