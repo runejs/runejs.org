f62b68d5357252ec5ce555435f6e284e0bb9508c
Tynarus <github@tyn.dev>
2020-11-23 15:57:53 -0600

    Adding initial home page and header + firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderModule } from './layout/header/header.module';
+import { FooterModule } from './layout/footer/footer.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
     declarations: [
@@ -12,7 +15,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     imports: [
         BrowserModule,
         AppRoutingModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        HeaderModule,
+        FooterModule,
+        HomeModule
     ],
     providers: [],
     bootstrap: [AppComponent]
---
+++ src/app/layout/header/header.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+    declarations: [HeaderComponent],
+    exports: [
+        HeaderComponent
+    ],
+    imports: [
+        CommonModule,
+        MatToolbarModule,
+        MatButtonModule,
+        RouterModule
+    ]
+})
+export class HeaderModule {
+}
